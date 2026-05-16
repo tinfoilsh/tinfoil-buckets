c01d05fc34ffd4e76464e23478c4052c5f20ce65
Daniel MS <danielmccannsayles@gmail.com>
2026-05-15 19:31:57 -0700

    Add cors so webapp can call buckets (#14)
    * add CORS
    
    * cleaner

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ func main() {
 
 	httpServer := &http.Server{
 		Addr:         cfg.ListenAddr,
-		Handler:      mux,
+		Handler:      corsMiddleware(mux),
 		ReadTimeout:  30 * time.Second,
 		WriteTimeout: 60 * time.Second,
 	}
